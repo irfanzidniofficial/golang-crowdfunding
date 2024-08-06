6e97d33dac6f847fcd61a0090c16f90866a3a921
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-08-06 21:43:51 +0700

    create users repo, service, handler register user api

---
+++ handler/user.go
@@ -0,0 +1,35 @@
+package handler
+
+import (
+	"golang-crowfunding/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
+
+}
